9e97d64ba8da12bc295395726dae305fc1769f6a
Aniket Dey <aniketdey2004@gmail.com>
2025-08-29 23:39:19 +0530

    views folder added for templates and index route for show all listings setup

--- app.js
+++ app.js
@@ -3,8 +3,11 @@ const app=express();
 const PORT=8080;
 const mongoose=require('mongoose');
 const Listing=require('./models/listing.js');
-
+const path=require("path");
 const MONGO_URL= 'mongodb://localhost:27017/wanderlust';
+
+app.set("view engine","ejs");
+app.set("views",path.join(__dirname,"/views"));
 
 main()
 .then(()=>{
@@ -20,19 +23,23 @@ async function main(){
 app.get("/",(req,res)=>{
     res.send("working");
 });
-
-app.get("/test",async (req,res)=>{
-    const sampleList=new Listing({
-        title:"My new Villa",
-        description:"By the beach",
-        price:1200,
-        location:"Calgante,Goa",
-        country:"India"
-    });
-    await sampleList.save();
-    console.log("data successfully saved");
-    res.send("saved");
-});
+//index route
+app.get("/listings",async (req,res)=>{
+    const allListings=await Listing.find({});
+    res.render("listings/index.ejs",{allListings});
+})
+// app.get("/test",async (req,res)=>{
+//     const sampleList=new Listing({
+//         title:"My new Villa",
+//         description:"By the beach",
+//         price:1200,
+//         location:"Calgante,Goa",
+//         country:"India"
+//     });
+//     await sampleList.save();
+//     console.log("data successfully saved");
+//     res.send("saved");
+// });
 
 app.listen(PORT,()=>{
     console.log(`Server is listening on ${PORT}`);
