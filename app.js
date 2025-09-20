9b12878d10e6329fbfe53aa5f97479aad51c4d93
Aniket Dey <aniketdey2004@gmail.com>
2025-09-20 14:53:57 +0530

    user model created,passport used for authentication,login and sign up page added

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+//dependencies
 const express=require('express');
 const app=express();
 const PORT=8080;
@@ -6,10 +7,16 @@ const path=require("path");
 const methodOverride=require('method-override');
 const ejsMate=require('ejs-mate');
 const ExpressError=require('./utils/ExpressError');
-const listings=require('./routes/listings.js');
-const reviews=require('./routes/reviews.js');
 const session=require('express-session');
 const flash=require('connect-flash');
+const passport=require('passport');
+const LocalStrategy=require('passport-local');
+const User=require('./models/user.js');
+
+//routers 
+const listingRouter=require('./routes/listings.js');
+const reviewRouter=require('./routes/reviews.js');
+const userRouter=require('./routes/user.js');
 
 const MONGO_URL= 'mongodb://localhost:27017/wanderlust';
 
@@ -26,6 +33,7 @@ async function main(){
     await mongoose.connect(MONGO_URL);
 }
 
+//middlewares
 app.set("view engine","ejs");
 app.set("views",path.join(__dirname,"/views"));
 app.use(express.urlencoded({extended:true}));
@@ -48,6 +56,15 @@ const sessionOptions={
 app.use(session(sessionOptions));
 app.use(flash());
 
+//authentication middlewares for passport
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+//flash messages
 app.use((req,res,next)=>{
     res.locals.success=req.flash("success");
     res.locals.error=req.flash("error");
@@ -58,13 +75,23 @@ app.get("/",(req,res)=>{
     res.send("working");
 });
 
+// app.get("/demouser",async (req,res)=>{
+//     let fakeUser={
+//         email:"aniketdey2004@gmail.com",
+//         username:"furious"
+//     }
+//     let registeredUser=await User.register(fakeUser,"helloworld");
+//     res.send(registeredUser);
+// });
 
 //listings routes
-app.use("/listings",listings);
+app.use("/listings",listingRouter);
 
 //reviews routes
-app.use("/listings/:id/reviews",reviews);
+app.use("/listings/:id/reviews",reviewRouter);
 
+//user routes
+app.use("/",userRouter);
 
 //Matching Unmatched Route
 app.all(/.*/,(req,res,next)=>{
